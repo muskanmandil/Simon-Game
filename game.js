e873e85e9b43d3b4d4cea5bc0a896d6a6b9d0fe4
Muskan Mandil <muskan05mandil@gmail.com>
2023-09-23 22:03:39 +0530

    updated project structure

--- game.js
+++ game.js
@@ -70,7 +70,7 @@ function nextSequence() {
 
 // Play Sound
 function playSound(name) {
-  var audio = new Audio("assets/" + name + ".mp3");
+  var audio = new Audio("assets/sounds/" + name + ".mp3");
   audio.play();
 }
 
